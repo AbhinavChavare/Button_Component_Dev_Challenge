b2c92e89f5c084368ea74f14bd13ee2ce3a6cb3f
AbhinavChavare <abhinavchavare07@gmail.com>
2023-01-03 10:33:22 +0530

    first commit Uploading to github

--- README.md
+++ README.md
@@ -1,70 +1,58 @@
-# Getting Started with Create React App
+<!-- Please update value in the {}  -->
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+<h1 align="center">{Buttons Component}</h1>
 
-## Available Scripts
+<div align="center">
+   Solution for a challenge from  <a href="http://devchallenges.io" target="_blank">Devchallenges.io</a>.
+</div>
 
-In the project directory, you can run:
+<div align="center">
+  <h3>
+    <a href="https://{your-demo-link.your-domain}">
+      Demo
+    </a>
+    <span> | </span>
+    <a href="https://{your-url-to-the-solution}">
+      Solution
+    </a>
+    <span> | </span>
+    <a href="https://devchallenges.io/challenges/ohgVTyJCbm5OZyTB2gNY">
+      Challenge
+    </a>
+  </h3>
+</div>
 
-### `npm start`
+<!-- TABLE OF CONTENTS -->
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+## Table of Contents
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
+  - [Built With](#built-with)
+- [Features](#features)
+- [How to use](#how-to-use)
+- [Contact](#contact)
+- [Acknowledgements](#acknowledgements)
 
-### `npm test`
+### Built With
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+<!-- This section should list any major frameworks that you built your project using. Here are a few examples.-->
 
-### `npm run build`
+- [React](https://reactjs.org/)
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
+## Features
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
+<!-- List the features of your application or follow the template. Don't share the figma file here :) -->
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+This application/site was created as a submission to a [DevChallenges](https://devchallenges.io/challenges) challenge. The [challenge](https://devchallenges.io/challenges/ohgVTyJCbm5OZyTB2gNY) was to build an application to complete the given user stories.
 
-### `npm run eject`
 
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
+## Acknowledgements
 
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
+<!-- This section should list any articles or add-ons/plugins that helps you to complete the project. This is optional but it will help you in the future. For exmpale -->
 
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
+- [Steps to replicate a design with only HTML and CSS](https://devchallenges-blogs.web.app/how-to-replicate-design/)
 
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
+## Contact
 
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+- Website [your-website.com](https://abc-button-dev-challenge.netlify.app/})
+- GitHub [@your-username](https://{github.com/your-usermame})
+- Twitter [@your-twitter](https://twitter.com/abhinavchavare7})
--- src/App.js
+++ src/App.js
@@ -1,24 +1,167 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { AiOutlineArrowUp, AiOutlineArrowDown, AiOutlineArrowLeft, AiOutlineArrowRight, AiOutlineShoppingCart } from "react-icons/ai";
+import { useEffect, useState } from 'react';
+import Footer from "./Footer/Footer"
 
 function App() {
+
+const [select,setSelect]=useState("default")
+const [classdata,setClassdata]=useState("btn-default")
+
+  const handleChange=(e)=>{
+    setSelect(e.target.value)
+  }
+
+  useEffect(()=>{
+
+    switch(select){
+      case "outline":setClassdata("btn-outline")
+      break;
+      case "text":setClassdata("btn-Text")
+      break;
+      case "primary":setClassdata("btn-primary ")
+      break;
+      case "success":setClassdata("btn-secondary")
+      break;
+      case "danger":setClassdata("btn-danger")
+      break;
+      case "smaller":setClassdata("btn-primary btn-small")
+      break;
+      case "larger":setClassdata("btn-primary btn-large")
+      break;
+      // case "default":setClassdata("btn-primary")
+      // break;
+      default:setClassdata("btn-default")
+      break;
+  
+    }
+  } ,[select])
+ 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="App button-comp ">
+      <h2 className='head-border'>Welcome to Button Component</h2>
+      <div className='btn-wraper'>
+        <div className='btn-all-comp'>
+          <div className='btn-container flex-center '>
+            <h4> Button </h4>
+            <button onClick={()=>alert(select)} className={`${classdata}`}>Click me</button>
+          </div>
+          <div className='btn-container'>
+          <h4> Please select to change </h4>
+            <div className='btn-data-all'>
+              <h4>Variant</h4>
+              <select onChange={(e)=>handleChange(e)} >
+                <option value="none" >Default</option>
+                <option value="outline" >Outline</option>
+                <option value="text" >Text</option>
+              </select>
+            </div>
+            <div className='btn-data-all'>
+              <h4>Color</h4>
+              <select onChange={(e)=>handleChange(e)}>
+                <option value="default" >Default</option>
+                <option value="primary" >Primary</option>
+                <option value="success" >Success</option>
+                <option value="danger" >Danger</option>
+              </select>
+            </div>
+            <div className='btn-data-all'>
+              <h4>Size</h4>
+              <select onChange={(e)=>handleChange(e)}>
+                <option value="default" >Default</option>
+                <option value="larger" >Larger</option>
+                <option value="smaller" >Smaller</option>
+              </select>
+            </div>
+          </div>
+
+
+        </div>
+        <div className='btn-comp'>
+          <section>
+              <div className='btn-container'>
+              <h4> Default {"<"}Button{">"}</h4>
+              <button onClick={()=>alert("Thanks for Clicking")} className='btn-default'>Click me</button>
+            </div>
+            <div className='btn-container'>
+              <h4> Text {"<"}Button{">"}</h4>
+              <button onClick={()=>alert("Thanks for Clicking")} className='btn-Text'>Click me</button>
+            </div>
+            <div className='btn-container'>
+              <h4> Outline {"<"}Button{">"}</h4>
+              <button onClick={()=>alert("Thanks for Clicking")} className='btn-outline'>Click me</button>
+            </div>
+          </section>
+
+          <section>
+            <div className='btn-container'>
+              <h4> Disable Box Shadow {"<"}Button{">"}</h4>
+              <button onClick={()=>alert("Thanks for Clicking")} >Click me</button>
+            </div>
+            <div className='btn-container'>
+              <h4> Disabled {"<"}Button{">"}</h4>
+              <button onClick={()=>alert("Thanks for Clicking")} disabled={true} className="btn-disable" >Click me</button>
+            </div>
+          </section>
+
+          <section>
+            <div className='btn-container'>
+              <h4>    {"<"}Button{">"}</h4>
+              <button onClick={()=>alert("Thanks for Clicking")} className='btn-primary '>Click me <AiOutlineArrowUp className='iconz' /> </button>
+            </div>
+            <div className='btn-container'>
+              <h4>  {"<"}Button{">"}</h4>
+              <button onClick={()=>alert("Thanks for Clicking")} className="btn-primary" >Click me<AiOutlineArrowDown className='iconz' /> </button>
+            </div>
+            <div className='btn-container'>
+              <h4>  {"<"}Button{">"}</h4>
+              <button onClick={()=>alert("Thanks for Clicking")} className="btn-primary" >Click me<AiOutlineArrowLeft className='iconz' /> </button>
+            </div>
+            <div className='btn-container'>
+              <h4>  {"<"}Button{">"}</h4>
+              <button onClick={()=>alert("Thanks for Clicking")} className="btn-primary" >Click me<AiOutlineArrowRight className='iconz' /> </button>
+            </div>
+            <div className='btn-container'>
+              <h4>  {"<"}Button{">"}</h4>
+              <button onClick={()=>alert("Thanks for Clicking")} className="btn-primary" >Click me<AiOutlineShoppingCart className='iconz' /> </button>
+            </div>
+          </section>
+          <section>
+            <div className='btn-container'>
+              <h4> Smalller {"<"}Button{">"}</h4>
+              <button onClick={()=>alert("Thanks for Clicking")} className='btn-primary btn-small'>Click me</button>
+            </div>
+            <div className='btn-container'>
+              <h4> Default {"<"}Button{">"}</h4>
+              <button onClick={()=>alert("Thanks for Clicking")} className='btn-primary'>Click me</button>
+            </div>
+            <div className='btn-container'>
+              <h4> Larger {"<"}Button{">"}</h4>
+              <button onClick={()=>alert("Thanks for Clicking")} className='btn-primary btn-large'>Click me</button>
+            </div>
+          </section>
+          <section>
+            <div className='btn-container'>
+              <h4> Primary {"<"}Button{">"}</h4>
+              <button onClick={()=>alert("Thanks for Clicking")} className='btn-primary '>Click me</button>
+            </div>
+            <div className='btn-container'>
+              <h4> Success {"<"}Button{">"}</h4>
+              <button onClick={()=>alert("Thanks for Clicking")} className='btn-secondary'>Click me</button>
+            </div>
+            <div className='btn-container'>
+              <h4> Danger {"<"}Button{">"}</h4>
+              <button onClick={()=>alert("Thanks for Clicking")} className='btn-danger'>Click me</button>
+            </div>
+          </section>
+        </div>
+      </div>
     </div>
+      <Footer/>
+    </>
   );
 }
 
